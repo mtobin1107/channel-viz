5628d97bf56443f5a2132513c62d33469bdce906
mtobin1107 <github@wirelesseco.com>
2014-06-17 11:15:00 -0700

    Update follows.js
    Add Key, Feed, and change title

--- follows.js
+++ follows.js
@@ -12,13 +12,13 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
-		applicationName	= '', // Replaces Xively logo in the header
+	var defaultKey		= '92HUr0pOawvhwV53z2IQ2cy76IHR5EZlMp2boCuf9yOC4hOX', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [943063070], // Comma separated array of Xively Feed ID numbers
+		applicationName	= 'EMPI TENS Monitor', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
-		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
+		hideForm		= 1; // To hide input form use value of 1, otherwise set to 0
 
 // Function Declarations
 
